8b4ed57b4b4ed18dd37e1b6490fc799cdf11498d
lakhan_nad <lakhannad1999@gmail.com>
2020-10-16 16:12:33 +0530

    Updated max_pairwise_product

--- Algorithms/Basic/max_pairwise_product.c
+++ Algorithms/Basic/max_pairwise_product.c
@@ -1,4 +1,6 @@
+/* Find pairwise maximum product of two numbers in array */
 #include <stdio.h>
+#include <limits.h>
 
 void swap(int *a, int *b) {
   int temp;
@@ -6,7 +8,14 @@ void swap(int *a, int *b) {
   *a = *b;
   *b = temp;
 }
+/*
+Find two maximum elements in array and take their product
+Find two minimum elements in array and take their product
 
+Print maximum of two products calculated above
+
+The minimum elements is taken o handle -ve * -ve case
+*/
 void main() {
   int n;
   scanf("%d", &n);
@@ -14,9 +23,10 @@ void main() {
   for (int i = 0; i < n; i++) {
     scanf("%d", &arr[i]);
   }
-  int max1 = -1;
-  int max2 = -1;
-  int temp;
+  int max1 = INT_MIN;
+  int max2 = INT_MIN;
+  int min1 = INT_MAX;
+  int min2 = INT_MAX;
   for (int i = 0; i < n; i++) {
     if (arr[i] > max2) {
       max2 = arr[i];
@@ -24,7 +34,17 @@ void main() {
     if (max2 > max1) {
       swap(&max1, &max2);
     }
+    if(arr[i] < min2){
+      min2 = arr[i];
+    }
+    if(min2 < min1){
+      swap(&min2, &min1);
+    }
   }
   long max_product = (long)max1 * (long)max2;
+  long temp = (long)min1 * (long)min2;
+  if(temp > max_product){
+    max_product = temp;
+  }
   printf("%ld", max_product);
 }
