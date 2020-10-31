53463c7ca5d8d9ff16c8dc1babee438ab9b11019
nitinsultania <nitinsultania3@gmail.com>
2020-10-31 21:23:52 +0530

    added Queue using Array

---
+++ Queue/queue_using_array.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+
+int front=-1,rear=-1;
+const int size=10;
+int queue[10];
+
+void enqueue(int data)
+{
+    if(rear==size-1)
+    {
+        printf("Queue is full\n");
+        return;
+    }
+    if(front==-1)
+        front=rear=1;
+    else
+        rear++;
+    queue[rear]=data;
+}
+
+int dequeue()
+{
+    if(rear==-1)
+    {
+        printf("Queue is empty\n");
+        return -99;
+    }
+    int data=queue[front];
+    if(front==rear)
+    {
+        front=rear=-1;
+    }
+    else
+    {
+        front++;
+    }
+    return data;
+}
+
+void display()
+{
+    if(front==-1)
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("The elements are: ");
+    for(int i=front;i<=rear;i++)
+    {
+        printf("%d ",queue[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    while(1)
+    {
+        printf("1.Enqueue\n");
+        printf("2.Dequeue\n");
+        printf("3.Display\n");
+        printf("4.Exit\n");
+        int choice;
+        scanf("%d",&choice);
+        int data;
+        switch(choice)
+        {
+            case 1:
+                printf("Enter the data: \n");
+                scanf("%d",&data);
+                enqueue(data);
+                break;
+            case 2:
+                data=dequeue();
+                if(data!=-99)
+                    printf("The data deleted is: %d\n", data);
+                break;
+            case 3:
+                display();
+                break;
+            default:printf("Thankyou\n");
+                return 0;
+        }
+    }
+    return 0;
+}
